981be30341b56b6c916e5d955e5d52fa871eb99f
Max Ovsyannikov <maxovsyannikov29@gmail.com>
2022-11-11 01:29:41 +0300

    чат начало

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # yandex_messenger
-yandex messenger
+
+JS + handlebars
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Running Development Mode
+
+```bash
+npm run dev
+```
+
+## Running build
+
+```bash
+npm run build
+```
+
+## Running express on localhost:3000
+
+```bash
+npm run start
+```
+
+## Figma resources
+
+- [Chart design](https://www.figma.com/file/24EUnEHGEDNLdOcxg7ULwV/Chat?node-id=0%3A1)
+
+## Netlify
+
+- [Netlify deploy](https://voluble-zabaione-0a1092.netlify.app/)
---
+++ src/index.js
@@ -0,0 +1,90 @@
+import Handlebars from "handlebars/dist/handlebars.runtime";
+
+import mainTemplate from "./pages/main/index.hbs";
+import chatTemplate from "./pages/chat/index.hbs";
+import signinTemplate from "./pages/signin/index.hbs";
+import signupTemplate from "./pages/signup/index.hbs";
+import errorTemplate from "./pages/error/index.hbs";
+import profileTemplate from "./pages/profile/index.hbs";
+
+import button from "./components/button/button.hbs";
+import input from "./components/input/input.hbs";
+import profileField from "./components/profileField/profileField.hbs";
+import profileButton from "./components/profileButton/profileButton.hbs";
+import avatar from "./components/avatar/avatar.hbs";
+
+const PAGES = {
+  main: {
+    template: mainTemplate,
+  },
+  chat: {
+    template: chatTemplate,
+  },
+  signin: {
+    template: signinTemplate,
+  },
+  signup: {
+    template: signupTemplate,
+  },
+  error500: {
+    template: errorTemplate,
+    data: {
+      title: "500",
+      text: "Мы уже фиксим",
+    },
+  },
+  error404: {
+    template: errorTemplate,
+    data: {
+      title: "400",
+      text: "Не туда попали",
+    },
+  },
+  profileInfo: {
+    template: profileTemplate,
+    data: {
+      view: "info",
+    },
+  },
+  profileUpdate: {
+    template: profileTemplate,
+    data: {
+      view: "update",
+    },
+  },
+  profilePassword: {
+    template: profileTemplate,
+    data: {
+      view: "password",
+    },
+  },
+};
+
+window.renderPage = (name, data) => {
+  const root = document.querySelector("#app");
+  const template = PAGES[name].template;
+  const params = data || PAGES[name].data;
+
+  const html = template(params);
+  root.innerHTML = html;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  Handlebars.registerPartial("button", button);
+  Handlebars.registerPartial("input", input);
+  Handlebars.registerPartial("profileField", profileField);
+  Handlebars.registerPartial("profileButton", profileButton);
+  Handlebars.registerPartial("avatar", avatar);
+
+  Handlebars.registerHelper("eq", (a, b) => a == b);
+  Handlebars.registerHelper("noteq", (a, b) => a !== b);
+
+  renderPage("main", {
+    pages: Object.keys(PAGES)
+      .filter((el) => el !== "main")
+      .map((el) => ({
+        children: el,
+        onclick: `renderPage('${el}')`,
+      })),
+  });
+});
